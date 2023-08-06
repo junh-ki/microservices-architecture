3d3e9acead5ccabaf4d18404555ff3e641781566
Junhyung Ki <kijoonh91@gmail.com>
2023-08-06 20:49:47 +0200

    Initialize the loans service

---
+++ loans/src/main/java/com/junhki/loans/LoansApplication.java
@@ -0,0 +1,11 @@
+package com.junhki.loans;
+
+import org.springframework.boot.SpringApplication;
+
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
